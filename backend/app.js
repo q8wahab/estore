72d79dadfadb11d850177e0259548e46ac1c3696
Abdulwahab Alhajji <q8wahab@gmail.com>
2025-07-10 03:23:37 +0300

    fix: connected frontend to backend on port 5050

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload"); // used for image and other files
 const path = require("path");
 const cors = require("cors");
-require("dotenv").config({ path: "./config/config.env" });
+require("dotenv").config({ path: "backend/config/config.env" });
 
 
 
@@ -39,10 +39,10 @@ app.use("/api/v1", payment);
 
 const __dirname1 = path.resolve();
 
-app.use(express.static(path.join(__dirname1, "/frotend/build")));
+app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
 app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname1, "frotend", "build", "index.html"))
+  res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
 );
 
 
